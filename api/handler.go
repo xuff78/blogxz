867a14fe936b4e6a3034476639dbc5d7f2deb33b
xuff78 <xuyichao@infohold.com.cn>
2019-03-29 14:52:38 +0800

    添加文件

---
+++ api/handler.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func createUser(w http.ResponseWriter, r *http.Request, p httprouter.Params)  {
+	
+}
--- api/main.go
+++ api/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
+func Prepare(){
+
+}
+
+func registerHandlers() *httprouter.Router{
+	router := httprouter.New()
+	router.POST("/addUser", createUser)
+	return router
+}
+
 func main()  {
-	
+	Prepare()
+	r:=registerHandlers()
+	http.ListenAndServe(":8001", r)
 }
