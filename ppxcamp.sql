27711cbbbf2db1f309bd8cdb94eb087716effdad
xuff78 <xuyichao@infohold.com.cn>
2019-08-04 17:45:24 +0800

    更新数据库

--- ppxcamp.sql
+++ ppxcamp.sql
@@ -1,24 +1,27 @@
-/*
-Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ppxcamp
+-- ------------------------------------------------------
+-- Server version	5.7.22
 
-Source Server         : localDB
-Source Server Version : 50722
-Source Host           : localhost:3306
-Source Database       : ppxcamp
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50722
-File Encoding         : 65001
+--
+-- Table structure for table `article`
+--
 
-Date: 2019-08-04 17:16:17
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for article
--- ----------------------------
 DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `article` (
   `id` varchar(64) NOT NULL,
   `cate_id` varchar(64) DEFAULT NULL,
@@ -29,15 +32,24 @@ CREATE TABLE `article` (
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of article
--- ----------------------------
+--
+-- Dumping data for table `article`
+--
 
--- ----------------------------
--- Table structure for category
--- ----------------------------
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
 DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `author_id` varchar(64) DEFAULT NULL,
@@ -46,15 +58,24 @@ CREATE TABLE `category` (
   `game_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of category
--- ----------------------------
+--
+-- Dumping data for table `category`
+--
 
--- ----------------------------
--- Table structure for comment
--- ----------------------------
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
 DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comment` (
   `id` varchar(64) NOT NULL,
   `reply_id` varchar(64) DEFAULT NULL,
@@ -65,15 +86,24 @@ CREATE TABLE `comment` (
   `time` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of comment
--- ----------------------------
+--
+-- Dumping data for table `comment`
+--
 
--- ----------------------------
--- Table structure for game
--- ----------------------------
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game`
+--
+
 DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `game` (
   `game_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(64) NOT NULL,
@@ -83,29 +113,47 @@ CREATE TABLE `game` (
   `author_name` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`game_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of game
--- ----------------------------
+--
+-- Dumping data for table `game`
+--
 
--- ----------------------------
--- Table structure for privilege
--- ----------------------------
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `privilege`
+--
+
 DROP TABLE IF EXISTS `privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `privilege` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of privilege
--- ----------------------------
+--
+-- Dumping data for table `privilege`
+--
 
--- ----------------------------
--- Table structure for sessions
--- ----------------------------
+LOCK TABLES `privilege` WRITE;
+/*!40000 ALTER TABLE `privilege` DISABLE KEYS */;
+/*!40000 ALTER TABLE `privilege` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessions`
+--
+
 DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sessions` (
   `session_id` tinytext NOT NULL,
   `TTL` tinytext,
@@ -113,37 +161,49 @@ CREATE TABLE `sessions` (
   `login_id` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`session_id`(64))
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of sessions
--- ----------------------------
+--
+-- Dumping data for table `sessions`
+--
 
--- ----------------------------
--- Table structure for userprivilege
--- ----------------------------
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userprivilege`
+--
+
 DROP TABLE IF EXISTS `userprivilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userprivilege` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) NOT NULL,
   `privilege_id` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of userprivilege
--- ----------------------------
-INSERT INTO `userprivilege` VALUES ('1', '11', '12');
-INSERT INTO `userprivilege` VALUES ('2', '11', '2');
-INSERT INTO `userprivilege` VALUES ('3', '11', '3');
-INSERT INTO `userprivilege` VALUES ('4', '12', '4');
-INSERT INTO `userprivilege` VALUES ('5', '12', '2');
-INSERT INTO `userprivilege` VALUES ('6', '12', '35');
-INSERT INTO `userprivilege` VALUES ('7', '12', '6');
+--
+-- Dumping data for table `userprivilege`
+--
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
+LOCK TABLES `userprivilege` WRITE;
+/*!40000 ALTER TABLE `userprivilege` DISABLE KEYS */;
+INSERT INTO `userprivilege` VALUES (1,11,12),(2,11,2),(3,11,3),(4,12,4),(5,12,2),(6,12,35),(7,12,6);
+/*!40000 ALTER TABLE `userprivilege` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
 DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(64) DEFAULT NULL,
@@ -152,13 +212,25 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', 'test', '123456', '测试');
-INSERT INTO `users` VALUES ('8', 'test2', '123456', '测试2');
-INSERT INTO `users` VALUES ('9', 'test3', '123456', '测试3');
-INSERT INTO `users` VALUES ('10', 'test4', '123456', '测试4');
-INSERT INTO `users` VALUES ('11', 'test5', '123456', '测试5');
-INSERT INTO `users` VALUES ('12', 'test6', '123456', '测试6');
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'test','123456','测试'),(8,'test2','123456','测试2'),(9,'test3','123456','测试3'),(10,'test4','123456','测试4'),(11,'test5','123456','测试5'),(12,'test6','123456','测试6');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-08-04 17:44:29
